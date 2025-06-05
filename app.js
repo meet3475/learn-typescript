f9689577715cfb070db8a7acf42d1ebf43911d09
meet3475 <meetdobariya480@gmail.com>
2025-06-05 17:05:44 +0530

    done first

---
+++ app.js
@@ -0,0 +1,6 @@
+var userName = "John Doe";
+var userAge = 30;
+var collage = "XYZ University";
+console.log(userName);
+console.log(userAge);
+console.log(collage);
---
+++ app.ts
@@ -0,0 +1,7 @@
+var userName: string = "John Doe";
+var userAge: number = 30;
+var collage: string = "XYZ University";
+
+console.log(userName);
+console.log(userAge);
+console.log(collage);
